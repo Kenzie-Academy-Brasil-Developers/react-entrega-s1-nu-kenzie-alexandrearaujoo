3703221a076d881ec6f7b9a0b70748ac39f76dc5
Alexandre Araujo <alexandre01393@gmail.com>
2022-02-09 09:18:13 -0300

    fix: update compenent TotalMoney

--- src/components/TotalMoney/index.jsx
+++ src/components/TotalMoney/index.jsx
@@ -1,12 +1,15 @@
 const TotalMoney = ({listTransactions}) => {
 
-    const total = listTransactions.reduce((acc,{value}) => acc + value, 0)
+
 
     return (
-
         <div className="valorTotal">
             <span>Valor total: </span>
-            <span>$ {total}</span>
+            <span>
+                {
+                `$: ${listTransactions.reduce((acc, {value, type}) => type === 'Entrada' ? acc + value : acc - value , 0)}`
+                } 
+            </span>
         </div>
     )
 }
